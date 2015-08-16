6356af8df06cc4552583d61d6e107ed2a2b49353
Ty Kelly <kellytyquan@gmail.com>
2015-08-16 05:07:42 -0400

    updated movies/index.html.erb and initializers/date_format.rb

--- config/initializers/date_format.rb
+++ config/initializers/date_format.rb
@@ -1,2 +1,2 @@
 # => "April 25th, 2007"
-Date::DATE_FORMATS[:default] = lambda { |date| day_format = ActiveSupport::Inflector.ordinalize(date.day); date.strftime("%B #{day_format}, %Y") }
+Date::DATE_FORMATS[:release_date] = lambda { |date| day_format = ActiveSupport::Inflector.ordinalize(date.day); date.strftime("%B #{day_format}, %Y") }
