b2f3b28af1f53541f25df485df30e3b2f7ebf572
Ty Kelly <kellytyquan@gmail.com>
2015-11-07 03:35:38 -0500

    added GenreController spec tests for non-admin logged in users

---
+++ spec/controllers/genres_controller_spec.rb
@@ -0,0 +1,43 @@
+describe GenresController do
+	
+	context	"when not logged in as admin" do
+
+		before do
+			@genre = Genre.create!(name: "Genre 1")
+			@user = User.create!(user_attributes(admin: false))
+			session[:user_id] = @user
+		end
+		
+		it "cannot access new action" do
+			get :new
+
+			expect(response).to redirect_to(root_url)
+		end
+
+		it "cannot access create action" do
+			post :create
+
+			expect(response).to redirect_to(root_url)
+		end
+
+		it "cannot access edit action" do
+			get :edit, id: @genre
+
+			expect(response).to redirect_to(root_url)
+		end
+
+		it "cannot access update action" do
+			patch :update, id: @genre
+
+			expect(response).to redirect_to(root_url)
+		end
+
+		it "cannot access destroy action" do
+			delete :destroy, id: @genre
+
+			expect(response).to redirect_to(root_url)
+		end
+	
+	end
+
+end
