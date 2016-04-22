dd261b1ab5fafcb192229629bd538421dc324983
Ty Kelly <kellytyquan@gmail.com>
2016-04-22 05:37:44 -0400

    changed app to work in production and development env

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ gem 'aws-sdk', '< 2.0'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 
+
 # Use unicorn as the app server
 # gem 'unicorn'
 
--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,4 +1,6 @@
-Paperclip::Attachment.default_options[:storage] = :s3
-Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-Paperclip::Attachment.default_options[:s3_credentials] = "#{Rails.root}/config/aws.yml"
+if Rails.env.production?
+	Paperclip::Attachment.default_options[:storage] = :s3
+	Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+	Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+	Paperclip::Attachment.default_options[:s3_credentials] = "#{Rails.root}/config/s3.yml"
+end
