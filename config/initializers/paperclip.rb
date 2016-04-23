891970762ac7824c0771f66f7cf46b2999052079
Ty Kelly <kellytyquan@gmail.com>
2016-04-23 00:30:24 -0400

    removed env specific initializer

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,6 +1,4 @@
-if Rails.env.production?
-	Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-	Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-	Paperclip::Attachment.default_options[:storage] = :s3
-	Paperclip::Attachment.default_options[:s3_credentials] = "#{Rails.root}/config/aws.yml"
-end
+Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+Paperclip::Attachment.default_options[:storage] = :s3
+Paperclip::Attachment.default_options[:s3_credentials] = "#{Rails.root}/config/aws.yml"
