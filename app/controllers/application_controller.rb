93194b69f58501ec6d69694160bdc8912a0cca3c
Ty Kelly <kellytyquan@gmail.com>
2018-01-20 01:59:46 -0500

    remove app controller code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,41 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :require_login
-
-  private
-
-  def require_login
-		unless logged_in?
-			session[:intended_url] = request.url
-			redirect_to login_url
-		end 
-  end
-
-  def current_user
-		@current_user ||= User.find_by(username: session[:user_id]) if session[:user_id]
-	end
-
-	def logged_in?
-		current_user != nil
-	end
-
-	def current_user?(user)
-		user.username == current_user.username
-	end
-
-	def require_admin_user
-    unless current_user.admin?
-      redirect_to root_url, alert: "Unauthorized Access!"
-    end
-  end
-
-  def current_user_admin?
-  	current_user && current_user.admin?
-  end
-
-	helper_method :current_user
-	helper_method :current_user?
-	helper_method :require_admin_user
-	helper_method :current_user_admin?
 end
