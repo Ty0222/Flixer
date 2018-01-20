aade8d9aeafaef9dd5ae75e1f6ba08e6041c7f4f
Ty Kelly <kellytyquan@gmail.com>
2018-01-20 01:55:12 -0500

    update urls for movie resource and remove routes for removed resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
-  
-  resources :genres, except: :show
+  root "movies#index"
+    
 	get "/signup" => "users#new"
 	get "/login" => "sessions#new"
-	resources :users
   resource :session
-  root "movies#index"
-  get "/genres/:id" => "movies#index_all_from_genre", as: :genre_movies
-  get "movies/filter/:scope" => "movies#index", as: :filtered_movies
-  resources :movies do
-  	resources :reviews
-  	resources :favorites
-  end
+
+	resources :users
+
+  resources :genres, only: :index
+
+  get "/movies/catalog/page/:page" => "movies#index", as: :movies, defaults: { page: "1" }
+  get "/movies/catalog/filters/genres/:genre_ids/page/:page" => "movie_filters#index_by_genre",
+   as: :filtered_movies_by_genre, defaults: { page: "1" }
+  resources :movies
+
 end
