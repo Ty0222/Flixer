f6a7484bfb6f71efb969c81063996470f330df21
Ty Kelly <kellytyquan@gmail.com>
2018-01-20 01:29:15 -0500

    add note annotations for spec files and utility code to aid with rails autoloading

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,20 @@
 require File.expand_path('../boot', __FILE__)
-
+require File.expand_path('../../lib/utility/utility', __FILE__)
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Flix
+module Flixer
   class Application < Rails::Application
+
+    custom_paths = ["lib", "lib/data_access"]
+
+    # # Adds custom paths to autoload paths
+    Utility.merge_lists_of_paths(path_list: config.autoload_paths, paths_to_add: custom_paths)
+
+    SourceAnnotationExtractor.enumerate("TODO", dirs: %w(spec), tag: true) # directories: app, config, db, lib, test, spec
         
     console do
       ActiveRecord::Base.connection
