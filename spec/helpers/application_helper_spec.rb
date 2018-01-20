a6778e1e199c2a66c5ed81b8c9ca16a492ffd08a
Ty Kelly <kellytyquan@gmail.com>
2018-01-20 06:26:55 -0500

    add specs for get_movies_url

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,6 +2,28 @@ require "rails_helper"
 
 RSpec.describe ApplicationHelper, type: :helper do
   
+  describe "#get_movies_url" do
+
+    context "when there is movie list metadata" do  
+      it "returns metadata's url to a listing of movies" do
+        movie_list_metadata = double(page: 1)
+        allow(movie_list_metadata).to receive_message_chain(:movie_list_url).with(anything).and_return("METADATA_URL")
+        result = helper.get_movies_url(movie_list_metadata)
+        
+        expect(result).to eq("METADATA_URL")
+      end
+    end
+    
+    context "when there is no movie list metadata" do  
+      it "returns a default url to a listing of movies" do
+        empty_movie_list_metadata = nil
+        result = helper.get_movies_url(empty_movie_list_metadata)
+        
+        expect(result).to eq(movies_url(hit_status: false))
+      end
+    end
+  end
+
   describe "#get_hit_movies_url" do
 
     context "when there is movie list metadata" do  
