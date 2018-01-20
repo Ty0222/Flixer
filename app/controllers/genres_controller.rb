2c694d0ac22fc24f76443eb67268d749fa22d4ef
Ty Kelly <kellytyquan@gmail.com>
2018-01-20 02:05:17 -0500

    update controller for showing an index of genres, remove other actions

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -2,48 +2,9 @@ class GenresController < ApplicationController
 	before_action :require_login, except: [:index]
 	before_action :require_admin_user, except: [:index]
 	before_action :set_genre, only: [:edit, :update, :destroy]
-	
-	def new
-		@genre = Genre.new
-	end
-
-	def create
-		@genre = Genre.new(genre_params)
-		if @genre.save
-			redirect_to genres_url, notice: "New genre added!"
-		else
-			render :new
-		end
-	end
-
-	def edit
-	end
-
-	def update
-		if @genre.update(genre_params)
-			redirect_to genres_url, notice: "Genre successfully updated!"
-		else
-			render :edit
-		end
-	end
 
 	def index
 		@genres = Genre.all
-	end
-
-	def destroy
-		@genre.destroy
-		redirect_to genres_url, notice: "Genre successfully deleted!"
-	end
-
-	private
-
-	def genre_params
-		params.require(:genre).permit(:name)
-	end
-
-	def set_genre
-		@genre = Genre.find(params[:id])
 	end
 
 end
