dbc7168ccf550c38944d7ef6578db905b72ac0b9
Ty Kelly <kellytyquan@gmail.com>
2018-01-20 01:01:11 -0500

    remove models

--- app/models/characterization.rb
+++
@@ -1,4 +0,0 @@
-class Characterization < ActiveRecord::Base
-  belongs_to :movie, foreign_key: "movie_slug"
-  belongs_to :genre
-end
--- app/models/favorite.rb
+++
@@ -1,4 +0,0 @@
-class Favorite < ActiveRecord::Base
-  belongs_to :movie, foreign_key: "movie_slug"
-  belongs_to :user
-end
