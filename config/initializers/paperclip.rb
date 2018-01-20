117cd88960694b9ebdd24573682965bd5efc50ef
Ty Kelly <kellytyquan@gmail.com>
2018-01-20 01:02:10 -0500

    remove paperclip-related files

--- config/initializers/paperclip.rb
+++
@@ -1,4 +0,0 @@
-Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-Paperclip::Attachment.default_options[:storage] = :s3
-Paperclip::Attachment.default_options[:s3_credentials] = "#{Rails.root}/config/aws.yml"
