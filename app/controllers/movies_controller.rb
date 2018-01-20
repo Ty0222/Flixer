6592ced93d1603105ec610e06108d3b360813413
Ty Kelly <kellytyquan@gmail.com>
2018-01-20 02:04:22 -0500

    update controller for showing an individual and index of movies, remove other actions

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,73 +1,23 @@
 class MoviesController < ApplicationController
 	skip_before_action :require_login, only: [:show, :index, :index_all_from_genre]
 	before_action :require_admin_user, except: [:index_all_from_genre, :index, :show]
-	before_action :set_movie, except: [:index, :index_all_from_genre, :new, :create]
 	
-
 	def index
-		@movies = Movie.send(movies_scope)
-	end
+		movies = Movie.now_playing(page: params[:page], hit_status: params[:hit_status])
+		movies_metadata = Movie.movie_list_metadata(page: params[:page], hit_status: params[:hit_status])
 
-	def index_all_from_genre
-		@genre = Genre.find(params[:id])
-		@movies = Movie.all_from_genre(@genre)
+		@movies = MovieDecorator.decorate(movies)
+		@movies_metadata = MovieMetadataDecorator.decorate(movies_metadata)
 	end
 
 	def show
-		@fans = @movie.fans
-		@genres = @movie.genres
-		if current_user
-			@current_favorite = current_user.favorites.find_by(movie_slug: @movie)
-		end
-	end
-
-	def edit
-	end
-
-	def update
-		if @movie.update(movie_params)
-			redirect_to @movie, notice: "Movie successfully updated!"
-		else
-			render :edit
-		end
-	end
-
-	def new
-		@movie = Movie.new
-	end
-
-	def create
-		@movie = Movie.new(movie_params)
-		if @movie.save
-			redirect_to @movie, notice: "Movie successfully created!"
-		else
-			render :new
-		end
-	end
-
-	def destroy
-		@movie.destroy
-		redirect_to movies_url, notice: "Movie successfully deleted!"
+		get_movie
 	end
 
 	private
 
-	def movie_params
-		params.require(:movie).
-			permit(:title, :rating, :total_gross,
-			:description, :released_on, :cast,
-			:director, :duration, :image, genre_ids: [])		
+	def get_movie
+		@movie = MovieDecorator.decorate(Movie.get_movie(id: params[:id]))
 	end
-
-	def set_movie
-		@movie = Movie.find(params[:id])		
-	end
-
-	def movies_scope
-		if params[:scope].in? %w(upcoming recent flops hits)
-			params[:scope]
-		else
-			:released_movies
-		end
-	end
+	
 end
