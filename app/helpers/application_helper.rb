8072e42242de4c0c1a0d5d1adb5982f2f5c09063
Ty Kelly <kellytyquan@gmail.com>
2018-01-20 01:46:42 -0500

    add helper for filtering movies by hits only

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,9 @@ module ApplicationHelper
 			content_tag(:title, "Flixer")
 		end
 	end
+
+  def get_hit_movies_url(movies_metadata)
+    return movies_url(hit_status: true) unless movies_metadata
+    movies_metadata.movie_list_url(page: movies_metadata.page, hit_status: true, context: self)
+  end
 end
