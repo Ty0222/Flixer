8e2044e2bbcfe1304b91d0025af55d04d32f4f42
Ty Kelly <kellytyquan@gmail.com>
2018-01-20 01:21:29 -0500

    update attribute file to represent incoming JSON data

--- spec/support/attributes.rb
+++ spec/support/attributes.rb
@@ -1,34 +1,34 @@
-
-def movie_attributes(overrides = {})
+def movie_list_data(overrides = {})
 	{
+		id: 1,
 	  title: "Iron Man 2",
- 	  rating: "PG-13",
-    total_gross: 623933331.0,
- 	  description: "Six months after the events of Iron Man, Tony Stark is resisting calls by the United States government to hand over the Iron Man technology while also combating his declining health from the arc reactor in his chest. Meanwhile, rogue Russian scientist Ivan Vanko has developed the same technology in order to pursue a vendetta against the Stark family, in the process joining forces with Stark's business rival, Justin Hammer.",
- 	  released_on: "2010-05-07",
- 	  cast: "Ryan Reynolds",
- 	  director: "Tim Miller",
- 	  duration: "123 min",
- 	  image: open("#{Rails.root}/app/assets/images/ironman2.jpg"),
+ 	  release_date: "2010-05-07",
+ 	  poster_path: "/iron_man2.jpg",
+ 	  vote_average: 7.3,
+ 	  vote_count: 100,
+ 	  overview: "This is an overview."
 	}.merge(overrides)
 end
 
-def review_attributes(overrides = {})
+def movie_list_metadata(overrides = {})
 	{
-		stars: 3,
-		comment: "I laughed, I cried, I spilled my popcorn!",
-		location: "Middletown, New Jersey"
+		page: 1,
+		total_pages: 10
 	}.merge(overrides)
-
 end
 
-def user_attributes(overrides = {})
+def movie_data(overrides = {})
 	{
-		name: "Ty Kelly",
-		username: "Ty0222",
-		email: "kellytyquan@example.com",
-		password: "secret",
-		password_confirmation: "secret",
-		admin: true
+		revenue: 1056389228,
+		runtime: 152,
+		backdrop_path: "/iron_man2.jpg",
+		genres: [{id: 1, name: "Action"}, {id: 2, name: "Adventure"}]
+	}.merge(movie_list_data).merge(overrides)
+end
+
+def genre_list_data(overrides = {})
+	{
+		id: 1,
+		name: "Action"
 	}.merge(overrides)
 end
